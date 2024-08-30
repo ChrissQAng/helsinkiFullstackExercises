a67228d091d3bcd02ec523eda19eaa83b42b8b74
ChrissAngerer <goodomen@outlook.de>
2024-08-30 11:23:04 +0200

    1.5 done

--- part1.5/src/App.jsx
+++ part1.5/src/App.jsx
@@ -1,22 +1,21 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-  console.log("++", parts);
-  console.log("**", parts[1]);
-  console.log("--", parts[1].name);
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   const Header = (props) => {
     return (
@@ -58,13 +57,15 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <br />
       <br />
-      <Content parts={parts} />
+      <Content parts={course.parts} />
       <Total
         totalExercises={
-          parts[0].exercises + parts[1].exercises + parts[2].exercises
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
         }
       />
     </div>
